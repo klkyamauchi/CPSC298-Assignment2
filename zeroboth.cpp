a6c97a4e305a8325d6b9035a761d80f7381a1d71
Kaitlynn <klkyamauchi@gmail.com>
2021-01-30 18:09:06 -0800

    Update zeroboth.cpp

--- zeroboth.cpp
+++ zeroboth.cpp
@@ -1,1 +1,16 @@
 
+/*
+Implement a void function called zeroBoth that has two call-by-reference parameters, 
+both of which are variables of type int, and sets thevalues of both variables to 0.
+*/
+
+
+#include <iostream>
+#include <string>
+#include <vector>
+using namespace std;
+
+void zeroBoth(int& num1, int& num2){
+    num1 = 0;
+    num2 = 0;
+}
